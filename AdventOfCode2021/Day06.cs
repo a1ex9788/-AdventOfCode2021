6fcf255b46789acbf529b6d55aa1a8ef4244071c
Alejandro <alejandrocarrion1999@gmail.com>
2021-12-08 11:23:49 +0100

    Aproximación Día 6.

---
+++ AdventOfCode2021/Day06.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021
+{
+    public class Day06 : Solver
+    {
+        private readonly IEnumerable<int> fishes;
+
+        private const int ResetPeriod = 7;
+
+        public Day06(string input)
+        {
+            this.fishes = input.Split(',').Select(i => int.Parse(i));
+        }
+
+        public override long SolvePart1()
+        {
+            return 0;
+
+            long numberOfFishes = 0;
+
+            for (int fish = 0; fish < fishes.Count(); fish++)
+            {
+                numberOfFishes += CalculateNumberOfFishesInNDays(fishes.ElementAt(fish), 80);
+            }
+
+            return numberOfFishes;
+        }
+
+        private static long CalculateNumberOfFishesInNDays(int initialFishState, int numberOfDays)
+        {
+            if (numberOfDays < initialFishState)
+            {
+                return 1;
+            }
+
+            int numberOfCompletedPeriods = numberOfDays / ResetPeriod;
+            int numberOfLeftDaysForPeriod = numberOfDays % ResetPeriod;
+
+            long numberOfFishes = 0;
+
+            for (int d = numberOfDays; d > 0; d -= ResetPeriod)
+            {
+                numberOfFishes += CalculateNumberOfFishesInNDays(8, d);
+            }
+
+            return numberOfFishes;
+        }
+
+        public override long SolvePart2()
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2021Tests/Day06Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2021;
+using AdventOfCode2021Tests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2021Tests
+{
+    [TestClass]
+    public class Day06Tests : Tester
+    {
+        protected override Solver Solver => new Day06(Resources.Day06Input);
+
+        protected override string Part1Output => Resources.Day06Part1Output;
+        protected override string Part2Output => Resources.Day06Part2Output;
+
+        [TestMethod]
+        [DataRow("3,4,3,1,2", 5934)]
+        public void SolvePart1Test(string input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Day06(input).SolvePart1());
+        }
+
+        [TestMethod]
+        [DataRow("3,4,3,1,2", -1)]
+        public void SolvePart2Test(string input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Day06(input).SolvePart2());
+        }
+    }
+}
